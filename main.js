5db928a8c4e3ca0cbf2e1413a83099fe921fe08d
Jennilyn Howell <jennilynhowell@me.com>
2017-06-13 13:05:17 -0400

    make progress with buttons

--- main.js
+++ main.js
@@ -13,10 +13,7 @@
     },
     {
       'id': 'seven',
-      'text': '7',
-      'btnValue': function(){
-        return 7;
-      }
+      'text': '7'
     },
     {
       'id': 'eight',
@@ -94,6 +91,7 @@
     calNode.appendChild(buttonNode);
 
     var displayText = document.createElement('p');
+    displayText.setAttribute('class', 'button-val');
     displayText.textContent = calButtons[i].text;
     buttonNode.appendChild(displayText);
 
@@ -102,30 +100,165 @@
     } else if (calButtons[i].id === 'displayField') {
       buttonNode.setAttribute('class', 'displayButton');
     } else if (calButtons[i].id === 'equals') {
-      buttonNode.setAttribute('class', 'equalsButton')
+      buttonNode.setAttribute('class', 'inputButton');
     };
 
   };
 
   //===================Let's press some buttons:
-  var inputButtons = document.querySelector('.calculator');
-  var clickedButton;
-  inputButtons.addEventListener('click', getButtonValue);
-  for (let i = 0; i <= inputButtons.length; i++) {
-    inputButtons[i].addEventListener('click', getButtonValue);
-  };
-
-  var equals = document.getElementById('equals');
-  equals.addEventListener('click', getButtonValue);
-
+  var inputButtons = document.querySelectorAll('.inputButton');
   var display = document.getElementById('displayField');
 
-  function getButtonValue(e) {
-    clickedButton = e.target.textContent;
-    display.textContent = clickedButton;
-    console.log(clickedButton);
-    return clickedButton;
-  }
+  // inputButtons.addEventListener('click', getButtonValue);
+  for (let i = 0; i <= inputButtons.length; i++) {
+    inputButtons[i].addEventListener('click', function(event){
+      var firstNumber;
+      var mathButton;
+      var secondNumber;
+      var storedButtons = [];
+      var clickedButton = event.target.textContent;
+
+      if (clickedButton === 'C') {
+        display.textContent = '';
+      } else if (clickedButton === '=') {
+        display.textContent += clickedButton;
+      } else if (clickedButton === '.') {
+        display.textContent += clickedButton;
+      } else if (clickedButton === 'x' || clickedButton === '/' || clickedButton === '+' || clickedButton === '-') {
+        display.textContent += clickedButton;
+        console.log(clickedButton);
+      }
+
+    })
+  };
+
+  // var equals = document.getElementById('equals');
+  // equals.addEventListener('click', getButtonValue);
+
+
+
+//   function getButtonValue(e) {
+//     var firstNumber;
+//     var secondNumber;
+//     var storedButtons = [];
+//     var mathButtons;
+//     var clickedButton;
+//     var displayContent;
+//
+//
+//     clickedButton = e.target.textContent;
+//     display.textContent = displayContent;
+//     console.log('clickedButton: ', clickedButton);
+//
+//     if (clickedButton === 'C') {
+//       display.textContent = '';
+//       firstNumber;
+//       secondNumber;
+//       mathButtons;
+//       storedButtons = [];
+//       clickedButton = '';
+//       console.log(storedButtons);
+//     };
+//
+//     //capture first number
+//     if (clickedButton !== 'C' && clickedButton !== 'x' && clickedButton !== '/' && clickedButton !== '+' && clickedButton !== '-') {
+//       display.textContent = clickedButton;
+//       storedButtons[0] = clickedButton;
+//       console.log(storedButtons);
+//
+//     }
+//
+//     //capture operator
+//     if (clickedButton === 'x' || clickedButton === '/' || clickedButton === '+' || clickedButton === '-') {
+//       display.textContent = clickedButton;
+//       storedButtons[1] = clickedButton;
+//       console.log(storedButtons);
+//     }
+//
+//     //capture second number
+//     if ((mathButtons) && (display.textContent !== 'C' && display.textContent !== 'x' && display.textContent !== '/' && display.textContent !== '+' && display.textContent !== '-')) {
+//       secondNumber = display.textContent;
+//       console.log('secondNumber: ', secondNumber);
+//       console.log(storedButtons);
+//       return secondNumber;
+//     }
+//
+//     console.log(storedButtons);
+//     var equation = firstNumber + " " + mathButtons + " " + secondNumber;
+//     console.log(equation);
+//     return equation;
+//
+//
+//
+// }
+
+
+
+    // function captureFirst(){
+    //   //capture first number
+    //
+    //   if (display.textContent !== 'C' && display.textContent !== 'x' && display.textContent !== '/' && display.textContent !== '+' && display.textContent !== '-') {
+    //     display.textContent = firstNumber;
+    //     storedButtons.push(firstNumber);
+    //     console.log('firstNumber: ', firstNumber);
+    //     return firstNumber;
+    //   };
+    // }
+    // var one = captureFirst();
+    //
+    // if (one){
+    //   function captureOperator() {  //capture math operator
+    //     if (display.textContent === 'x' || display.textContent === '/' || display.textContent === '+' || display.textContent === '-') {
+    //       display.textContent = mathButtons;
+    //       storedButtons.push(mathButtons);
+    //       console.log('mathButtons: ', mathButtons);
+    //       console.log('stored:', storedButtons);
+    //       return mathButtons;
+    //     };
+    //   }
+    //   var op = captureOperator();
+    // }
+    //
+    // if (one && op){
+    //   function captureSecond(){  //capture second number
+    //     if (display.textContent !== 'C' && display.textContent !== 'x' && display.textContent !== '/' && display.textContent !== '+' && display.textContent !== '-') {
+    //       display.textContent = secondNumber;
+    //       storedButtons.push(secondNumber);
+    //       console.log('secondNumber: ', secondNumber);
+    //       return secondNumber;
+    //     };
+    //   }
+    //
+    //   var two = captureSecond();
+    // }
+
+
+    // console.log(storedButtons);
+
+    // captureEquation();
+
+    // if (clickedButton) {
+    //   firstNumber = clickedButton;
+    //   storedButtons = clickedButton;
+    //   console.log('firstNumber: ', firstNumber);
+    //   console.log('storedButtons: ', storedButtons);
+    // } else if (clickedButton === 'x' || clickedButton === '/' || clickedButton === '+' || clickedButton === '-') {
+    //   display.textContent = clickedButton;
+    //   mathButtons = clickedButton;
+    //   console.log('mathButtons: ', mathButtons);
+    // }
+
+    // if (clickedButton === 'C') {
+    //   display.textContent = '';
+    // }
+
+
+    // console.log(clickedButton);
+    // console.log(storedButtons);
+    // return clickedButton;
+  // }
+
+
 
 
   //================================This is the calculator piece
@@ -135,40 +268,40 @@
       // pickOne = parseFloat(pickOne);
       // console.log(pickOne);
       // return pickOne;
-    var displayContent = display.textContent;
-    var firstNumber = '';
-    var secondNumber = '';
-    var operator = '';
-    var error = function() {
-      display.textContent = 'error';
+    // var displayContent = display.textContent;
+    // var firstNumber = '';
+    // var secondNumber = '';
+    // var operator = '';
+    // var error = function() {
+    //   display.textContent = 'error';
+    // // };
+    //
+    // var pickFirstNumber = function() {
+    //   if (displayContent === ''){
+    //     return;
+    //   }
+    //   firstNumber = parseFloat(display.textContent);
+    //   console.log(firstNumber);
+    //   return firstNumber;
+    //
+    //   // var pickOne = prompt("Please enter a number");
+    //   // pickOne = parseFloat(pickOne);
+    //   // console.log(pickOne);
+    //   // return pickOne;
     // };
 
-    var pickFirstNumber = function() {
-      if (displayContent === ''){
-        return;
-      }
-      firstNumber = parseFloat(display.textContent);
-      console.log(firstNumber);
-      return firstNumber;
-
-      // var pickOne = prompt("Please enter a number");
-      // pickOne = parseFloat(pickOne);
-      // console.log(pickOne);
-      // return pickOne;
-    };
-
-    var heyThisIsFirst = pickFirstNumber();
-    console.log(heyThisIsFirst);
-
-
-    var pickOperator = function() {
-       if (displayContent !== '') {
-         operator = display.textContent;
-         console.log(operator);
-         return operator;
-       }
-       return error();
-     }
+    // var heyThisIsFirst = pickFirstNumber();
+    // console.log(heyThisIsFirst);
+    //
+    //
+    // var pickOperator = function() {
+    //    if (displayContent !== '') {
+    //      operator = display.textContent;
+    //      console.log(operator);
+    //      return operator;
+    //    }
+    //    return error();
+    //  }
       // var choice = prompt("Choose +, -, *, or /");
       //
       // if (choice === '+') {
@@ -185,36 +318,37 @@
     // };
     // var doThisMath = pickOperator();
 
-    var pickSecondNumber = function() {
-      if (displayContent !== '') {
-        secondNumber = parseFloat(display.textContent);
-        console.log(secondNumber);
-        return secondNumber;
-      }
-      return error();
+    // var pickSecondNumber = function() {
+    //   if (displayContent !== '') {
+    //     secondNumber = parseFloat(display.textContent);
+    //     console.log(secondNumber);
+    //     return secondNumber;
+    //   }
+    //   return error();
       // var pickTwo = prompt("Please enter a number");
       // pickTwo = parseFloat(pickTwo);
       // console.log(pickTwo);
       // return pickTwo;
-    };
+    // };
 
     // var secondNumber = pickSecondNumber();
     //
-    function calculate(num1, op, num2) {
-      if (op === '+') {
-        return num1 + num2;
-      } else if (op === '-'){
-        return num1 - num2;
-      } else if (op === '*') {
-        return num1 * num2;
-      } else {
-        return num1 / num2;
-      };
-    }
+  //   function calculate(num1, op, num2) {
+  //     if (op === '+') {
+  //       return num1 + num2;
+  //     } else if (op === '-'){
+  //       return num1 - num2;
+  //     } else if (op === '*') {
+  //       return num1 * num2;
+  //     } else {
+  //       return num1 / num2;
+  //     };
+  //   }
+  //
+  //   var result = calculate(firstNumber, operator, secondNumber);
+  //   console.log(result);
+  // }
 
-    var result = calculate(firstNumber, operator, secondNumber);
-    console.log(result);
-  }
   //
   // }
   // calculator();
